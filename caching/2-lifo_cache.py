0ebfcf28e32544abe03c605e83a225689f960d23
bbel <bweb33@protonmail.com>
2024-09-26 16:02:31 +0200

    send

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -9,12 +9,14 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """ Initialize the class """
         super().__init__()
+        self.lastDataKeyPut=""
 
     def put(self, key, item):
         """ Add an item to the cache """
         if key is not None and item is not None:
             if key not in self.cache_data.keys() and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                self.outLastStack()
+                self.outLastItemPut(self.lastDataKeyPut)
+            self.lastDataKeyPut = key
             self.cache_data[key] = item
 
     def get(self, key):
@@ -23,7 +25,7 @@ class LIFOCache(BaseCaching):
             return self.cache_data.get(key)
         return None
 
-    def outLastStack(self):
+    def outLastItemPut(self,key):
         """ Remove the last item added to the cache (LIFO) """
-        deleteIndex = self.cache_data.popitem()
-        print("DISCARD: {}".format(deleteIndex[0]))
+        self.cache_data.pop(key)
+        print("DISCARD: {}".format(key))
