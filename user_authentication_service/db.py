2c89b747a5ebec54867fdd5930b041217f32448f
bbel <bweb33@protonmail.com>
2024-10-15 15:36:54 +0200

    send

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import User
+from user import User,Base
 
 class DB:
     """DB class
@@ -13,7 +13,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
