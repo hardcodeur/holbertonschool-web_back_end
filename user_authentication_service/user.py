22ea8e0104b7ec4aab1293d125ee16e4822427ab
bbel <bweb33@protonmail.com>
2024-10-19 17:12:14 +0200

    test

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""User module for ORM
+"""
+User module for ORM.
+
 This module defines the User class for SQLAlchemy ORM, representing a user in the database.
 """
 
@@ -8,14 +10,21 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class User(Base):
-    """User class
-    This class represents the 'users' table in the database.
     """
+    User class representing the 'users' table in the database.
+    
+    Attributes:
+        id (int): The primary key, automatically incremented.
+        email (str): The user's email, which cannot be null.
+        hashed_password (str): The user's hashed password, which cannot be null.
+        session_id (str | None): The session ID associated with the user, nullable.
+        reset_token (str | None): The reset token for password recovery, nullable.
+    """
+    
     __tablename__ = 'users'
 
-    id: int = Column(Integer, primary_key=True,autoincrement=True)
+    id: int = Column(Integer, primary_key=True, autoincrement=True)
     email: str = Column(String(250), nullable=False)
     hashed_password: str = Column(String(250), nullable=False)
     session_id: str | None = Column(String(250), nullable=True)
