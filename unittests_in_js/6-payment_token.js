1ed7f76e1f0014e1898a856d4ae7ce62d7ccd4ef
bbel <bweb33@protonmail.com>
2024-11-15 12:41:09 +0100

    end exo 9

--- unittests_in_js/6-payment_token.js
+++ unittests_in_js/6-payment_token.js
@@ -2,7 +2,6 @@ const getPaymentTokenFromAPI = (success)=>{
     if(success){
         return Promise.resolve({ data: 'Successful response from the API' });
     }
-    return Promise.resolve({})
 }
 
 module.exports = getPaymentTokenFromAPI
