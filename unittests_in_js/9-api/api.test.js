64bcdac38c20ec358a1318598e7afb7f9e0fb665
bbel <bweb33@protonmail.com>
2024-11-17 16:26:37 +0100

    fin

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,38 +1,38 @@
 const assert = require('assert');
 const request = require('request');
 
-describe("API",()=>{
+describe("API", () => {
 
-    describe('#router.get(/)',()=>{
-        it("Should return status code 200",(done)=>{
-            request.get("http://localhost:7865",(error, response, body)=>{
-                assert.equal(response.statusCode,200)
+    describe('#router.get(/)', () => {
+        it("Should return status code 200", (done) => {
+            request.get("http://localhost:7865", (error, response, body) => {
+                assert.equal(response.statusCode, 200)
                 done();
             });
         });
-    
-        it("Should return welcome sentence",(done)=>{
-            request.get("http://localhost:7865",(error, response, body)=>{
-                assert.equal(body,"Welcome to the payment system")
+
+        it("Should return welcome sentence", (done) => {
+            request.get("http://localhost:7865", (error, response, body) => {
+                assert.equal(body, "Welcome to the payment system")
                 done();
             });
         });
-    
+
     })
 
-    describe("#router.get(/card/:id)",()=>{
-        it("Should return status code 200 with id parameter is number",(done)=>{
+    describe("#router.get(/cart/:id)", () => {
+        it("Should return status code 200 with id parameter is number", (done) => {
             let id = 10;
-            request.get(`http://localhost:7865/card/${id}`,(error, response, body)=>{
-                assert.equal(response.statusCode,200);
+            request.get(`http://localhost:7865/cart/${id}`, (error, response, body) => {
+                assert.equal(response.statusCode, 200);
                 done();
             });
         });
 
-        it("Should return status code 404 with id parameter string string",(done)=>{
+        it("Should return status code 404 with id parameter string", (done) => {
             let id = "bouh";
-            request.get(`http://localhost:7865/card/${id}`,(error, response, body)=>{
-                assert.equal(response.statusCode,404);
+            request.get(`http://localhost:7865/cart/${id}`, (error, response, body) => {
+                assert.equal(response.statusCode, 404);
                 done();
             })
         });
